02f7bcf42fc77369fdadf6b04f904907aee630b4
Chevola Andrew <Chevola-Andrew@rambler.ru>
2022-10-14 16:09:28 +0300

    add Examle003_Sum

---
+++ Examle003_Sum/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+int numberA = 10;
+int numberB = 5;
+int result = numberA + numberB;
+Console.Write("Сумма чисел равна ");
+Console.Write(result);
