17e5816607b16e8dd25494edad6f29206fea0a06
Chevola Andrew <Chevola-Andrew@rambler.ru>
2022-10-14 16:19:39 +0300

    add Example004_Div

---
+++ Example004_Div/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+double numberA = 11;
+double numberB = 5;
+double result = numberA / numberB;
+Console.Write("Частное чисел равно ");
+Console.Write(result);
