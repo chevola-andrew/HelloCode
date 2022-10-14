896dbd4363fcab3422a255d3ead87ec757364744
Chevola Andrew <Chevola-Andrew@rambler.ru>
2022-10-14 16:51:22 +0300

    add Example005_ConditionIfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя.");
+string UserName = Console.ReadLine();
+
+if (UserName.ToLower() == "маша")
+    {
+    Console.Write("Ура! Это же Маша!!!");
+    }
+else
+    {
+    Console.Write("Привет, ");
+    Console.Write(UserName);
+    Console.Write("!");
+    }
