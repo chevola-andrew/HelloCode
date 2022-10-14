8e17428ae83f77b5bf6a177191f6ea53b4770e25
Chevola Andrew <Chevola-Andrew@rambler.ru>
2022-10-14 16:34:30 +0300

    add Random in Example003_Sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int numberA = 10;
-int numberB = 5;
+int numberA = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 10);
 int result = numberA + numberB;
-Console.Write("Сумма чисел равна ");
+Console.Write("Сумма чисел ");
+Console.Write(numberA);
+Console.Write(" и ");
+Console.Write(numberB);
+Console.Write(" равна ");
 Console.Write(result);
