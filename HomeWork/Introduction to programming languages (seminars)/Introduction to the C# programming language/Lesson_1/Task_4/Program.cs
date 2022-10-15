b3deedca1d9718c4224adc7b7112b2dfd0ef8ae6
Chevola Andrew <Chevola-Andrew@rambler.ru>
2022-10-15 15:33:37 +0300

    Task_4 is gooood

---
+++ HomeWork/Introduction to programming languages (seminars)/Introduction to the C# programming language/Lesson_1/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+Console.Write("Введите первое целое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье целое число: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+
+if (n1 == n2 && n1 == n3) Console.Write("Введенные числа равны.");
+   
+if (n2 > max) max = n2;
+
+if (n3 > max) max = n3;
+    
+Console.Write("Максимальное число: " + max);
